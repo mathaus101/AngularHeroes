fdd9e9d641c81ee70efdbdf38717fc10f7261e8b
Matthew Brooks <mbrooks@bendigohealth.org.au>
2019-04-30 17:43:24 +1000

    Angular Tutorial. 4 - Services. Finished creating HeroService injectable

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -15,6 +15,7 @@ export class HeroService {
 
   getHeroes(): Hero[] {
 
+    //console.log(HEROES);
     return HEROES;
 
   }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -13,22 +13,48 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  //You define private styles either inline in the @Component.styles array or as stylesheet file(s) identified in the @Component.styleUrls array.
+  //You define private styles either inline in the @Component.styles array or as stylesheet file(s) identified
+  // in the @Component.styleUrls array.
 
   constructor(private heroService: HeroService) {   // Declares a dependency 
 
+    // While you could call getHeroes() in the constructor, that's not the best practice.
+
+    // Reserve the constructor for simple initialization such as wiring constructor parameters to properties. 
+    // The constructor shouldn't do anything. It certainly shouldn't call a function that makes HTTP requests 
+    // to a remote server as a real data service would.
   }   
 
   ngOnInit() {
+    //console.log('init hereos component');
     this.getHereos();
   }
 
   onSelect(hero: Hero): void {
+
+    //To Do: When you find out how to pass the event args e, set status to running and pause only when it ends
+    // That should prevent jitter when clicking between links before the animation ends.
+
+    // $("#thing").hover(function() {
+      
+    //   this.style.webkitAnimationPlayState = "running";
+    
+    //   $(this).on('webkitAnimationEnd', function() {
+    //     this.style.webkitAnimationPlayState = "paused";
+    //   });
+
+
     this.selectedHero = hero;
+    var element = document.querySelector('#angularLogo');
+    element.classList.remove('logoSpin');    
+    void element.offsetWidth;
+    element.classList.add('logoSpin');
+
   }
 
   getHereos(): void {
     this.heroes = this.heroService.getHeroes();
+    //console.log(this.heroService.getHeroes());
 
   }
     
